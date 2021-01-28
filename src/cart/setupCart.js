5a395b5f36c366ad78808f1dd0f5a5f75090f008
fyardlest <fyardlest1@gmail.com>
2021-01-28 14:55:18 -0500

    Update Items

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -28,6 +28,10 @@ export const addToCart = (id) => {
   }
   else {
     // update values
+    const amount = increaseAmount(id)
+    const items = [...cartItemsDOM.querySelectorAll('.cart-item-amount')]
+    const newAmount = items.find(value => value.dataset.id === id)
+    newAmount.textContent = amount
   }
   // add one to the item count
   displayCartItemCount()
@@ -57,6 +61,18 @@ function displayCartItemsDOM() {
   cart.forEach(cartItem => {
     addToCartDOM(cartItem)
   });
+}
+
+function increaseAmount(id) {
+  let newAmount;
+  cart = cart.map((cartItem) => {
+    if(cartItem.id === id) {
+      newAmount = cartItem.amount + 1
+      cartItem = {...cartItem, amount: cartItem.amount + 1}
+    }
+    return cartItem
+  })
+  return newAmount
 }
 
 function setupCartFunctionality() {
